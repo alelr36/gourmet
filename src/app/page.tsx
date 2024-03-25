b5a564f77606f55712b9749d2f13ae0e50eb0ee4
Ale Arce <ale_lr36@hotmail.com>
2024-03-25 11:36:48 -0300

    [Ale] Formatter
    - Add prettier files and dependencies
    - Re-organize folders
    - format a few files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Card from '@/components/Card';
 
 export default function Home() {
-  return (
-    <div className="">
-      main
-    </div>
-  );
+	return (
+		<div className="">
+			<Card />
+		</div>
+	);
 }
