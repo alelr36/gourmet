df7da706f2a96b6491af04a0a0edc0e9f44e9110
Ale Arce <ale_lr36@hotmail.com>
2024-03-28 12:20:48 -0300

    [Ale] Update layout and theme
    - Update the way the theme is applied,
      now based on user setting
    - Update general layout and spacing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,10 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
+import { LoginLink, LogoutLink } from '@kinde-oss/kinde-auth-nextjs/components';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import prisma from '@/lib/db';
+import { getDefaultSettings } from '@/constants/settings';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,26 +20,50 @@ export default async function RootLayout({
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
-	const user = await prisma.user.findUnique({
-		where: { id: 1 },
-		include: { settings: true },
-	});
+	const { getUser, isAuthenticated } = getKindeServerSession();
+	const isAuthenticatedUser = await isAuthenticated();
+	const kindeUser = await getUser();
 
-	const { theme } = user?.settings || { theme: 'light' };
+	const settings = isAuthenticatedUser
+		? (
+				await prisma.user.findUnique({
+					where: {
+						kindeId: kindeUser?.id,
+					},
+					include: {
+						settings: true,
+					},
+				})
+			)?.settings
+		: getDefaultSettings();
 
 	return (
 		<html lang="en">
-			<body className={cn(inter.className, theme)}>
-				<div className="grid grid-cols-1 lg:grid-cols-12">
-					<aside className="p-4 flex flex-col items-center lg:col-span-2">
-						<Link href="/">Home</Link>
-						<Link href="/admin">Admin</Link>
-						<Link href="/login">Log In</Link>
-						<Link href="">Log Out</Link>
-					</aside>
-					<main className="min-h-screen p-4 flex flex-col items-center lg:col-span-10">
-						{children}
-					</main>
+			<body className={cn(inter.className, settings?.theme)}>
+				<div className="h-full">
+					<div className="grid grid-cols-1 lg:grid-cols-12">
+						<aside className="p-4 flex flex-col justify-between lg:h-screen md:sticky md:top-0 lg:col-span-2">
+							<div className="flex flex-col items-center">
+								<Link href="/">Home</Link>
+								{isAuthenticatedUser && (
+									<>
+										<Link href="/user/settings">Settings</Link>
+										<Link href="/admin">Admin</Link>
+									</>
+								)}
+							</div>
+							<div className="flex flex-col items-center">
+								{isAuthenticatedUser ? (
+									<LogoutLink>Log out</LogoutLink>
+								) : (
+									<LoginLink>Sign in</LoginLink>
+								)}
+							</div>
+						</aside>
+						<main className="p-4 flex flex-col items-center lg:col-span-10">
+							{children}
+						</main>
+					</div>
 				</div>
 			</body>
 		</html>
